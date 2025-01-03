d7e5cba70a2452e55a272adf4254b96919b086e9
Oleksii Musinov <a_musinov@yahoo.com>
2025-01-03 12:08:51 -0500

    to do list initial project commit

---
+++ src/components/search-panel.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const SearchPanel = () => {
+  const searchStyle = {
+    fontSize: '20px'
+  }
+  return (
+    <input 
+      placeholder="search"
+      style={searchStyle}
+    />
+  )
+}
+
+export default SearchPanel
---
+++ src/components/todo-list.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const TodoList = () => {
+	return (
+		<div>
+			<ul>
+				<li>Learn React</li>
+				<li>Build App</li>
+				<li>Do Something</li>
+			</ul>
+		</div>
+  )
+}
+
+export default TodoList
